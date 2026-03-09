b2aafc75c02734faebcad4a309b4af68e85d47df
2024tm93613 <2024tm93613@wilp.bits-pilani.ac.in>
2026-03-09 19:30:54 +0530

    Fix: Added /members endpoint to pass pytest

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "ACEest DevOps Application - Version 1"
+    return "ACEest DevOps Application"
+
+@app.route("/members")
+def members():
+    return jsonify({
+        "members": [
+            "Ramesh",
+            "Suresh",
+            "Mahesh"
+            "John"
+        ]
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
