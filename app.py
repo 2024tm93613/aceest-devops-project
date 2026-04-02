7b75f13d369a1066ea4865aaa41cb2d3160c4805
2024tm93613 <2024tm93613@wilp.bits-pilani.ac.in>
2026-04-02 19:08:26 +0530

    v3 - Added DB SQL Lite

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
+import sqlite3
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 programs = ["Fat Loss", "Muscle Gain", "Beginner"]
 
+def init_db():
+    conn = sqlite3.connect("aceest.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS clients (name TEXT, program TEXT)")
+    conn.commit()
+    conn.close()
+
 @app.route("/")
 def home():
-    return "ACEest DevOps Application v3"
+    return "ACEest DevOps Application v4"
 
 @app.route("/members")
 def members():
@@ -18,7 +26,6 @@ def members():
 def get_programs():
     return jsonify({"programs": programs})
 
-# NEW
 @app.route("/generate", methods=["POST"])
 def generate():
     data = request.get_json(silent=True)
@@ -26,16 +33,8 @@ def generate():
     if not data:
         return jsonify({"error": "Invalid JSON"}), 400
 
-    name = data.get("name")
-    program = data.get("program")
-
-    if not name or not program:
-        return jsonify({"error": "Missing fields"}), 400
-
-    return jsonify({
-        "message": f"Plan generated for {name}",
-        "program": program
-    })
+    return jsonify({"message": "Plan generated"})
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
