26c631ed1a9e495235d18ee3d80014a2e04b70d5
2024tm93613 <2024tm93613@wilp.bits-pilani.ac.in>
2026-04-02 19:02:46 +0530

    v3 - Issue Fixed - added generate API

--- app.py
+++ app.py
@@ -2,15 +2,35 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+programs = ["Fat Loss", "Muscle Gain", "Beginner"]
+
 @app.route("/")
 def home():
     return "ACEest DevOps Application v3"
 
+@app.route("/members")
+def members():
+    return jsonify({
+        "members": ["Ramesh", "Suresh", "Mahesh", "John"]
+    })
+
+@app.route("/programs")
+def get_programs():
+    return jsonify({"programs": programs})
+
+# NEW
 @app.route("/generate", methods=["POST"])
 def generate():
-    data = request.json
+    data = request.get_json(silent=True)
+
+    if not data:
+        return jsonify({"error": "Invalid JSON"}), 400
+
     name = data.get("name")
     program = data.get("program")
+
+    if not name or not program:
+        return jsonify({"error": "Missing fields"}), 400
 
     return jsonify({
         "message": f"Plan generated for {name}",
