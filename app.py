88ac41fe795d44da98acf52228fa24ae959c2f8b
2024tm93613 <2024tm93613@wilp.bits-pilani.ac.in>
2026-04-03 17:38:21 +0530

    v2: added programs GET API

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 # ✅ define empty list
 members_list = []
+programs = ["Yoga", "Cardio", "Strength"]
 
 @app.route("/")
 def home():
@@ -12,6 +13,10 @@ def home():
 @app.route("/members")
 def members():
     return jsonify({"members": members_list})
+
+@app.route("/programs")
+def get_programs():
+    return jsonify({"programs": programs})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
