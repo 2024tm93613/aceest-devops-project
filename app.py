68cf12187ec257658fe29aafcaf490e03ec501a0
2024tm93613 <2024tm93613@wilp.bits-pilani.ac.in>
2026-04-03 17:19:52 +0530

    v1 : Initial Commit after added pytest and basic test cases

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Welcome to ACEest Fitness & Gym"
 
+@app.route("/members")
+def members():
+    return jsonify({"members": members_list})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
